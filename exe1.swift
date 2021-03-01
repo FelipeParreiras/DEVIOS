d100515a7e598e48f8a2ce90592478e9a040b4a8
FelipeParreiras <77993189+FelipeParreiras@users.noreply.github.com>
2021-03-01 19:54:59 -0300

    Add files via upload

---
+++ exe1.swift
@@ -0,0 +1,24 @@
+// 01)
+var nomeUsuario = "Felipe"
+var nomeUsuario: String = "Felipe"
+var listaInts:[Int] = []
+var listaInts:[Int] = [1,2,3,4]
+var listaInts = [1,2,3,4]
+var X = 0.0, Y = 0.0, Z = 0.0
+var listaCompras = ["Ovos","Leite"]
+//02)
+let numeroChassi = "5za 2mzeJB bN 2b6779"
+var modeloCarro = "Camaro"
+var valorCarro = 377.900,00
+var anoCarro = 2020
+//03)
+// As anotações de tipo no swith são usadas para declara qual o tipo da variavel por exemplo, String, Int...
+//04)
+var intValue = 6
+var doubleValue = 6.5
+var valueString = String(intValue)
+var valueString = String(doubleValue)
+//05) type safe: 
+// Uma linguagem de tipo safe faz você ser específico sobre os tipos de valores com os quais seu código pode trabalhar. 
+//type inference:
+//Uma linguagem de tipo inference não faz você especificar o tipo do valor de que precisa, o Swift descobri o tipo apropriado.
